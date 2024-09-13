30e29495273fefcc37ac80060e08afcf6cb97ce9
Haorsis <cadetgauthier3@gmail.com>
2024-09-13 15:56:34 +0200

    - wip : portefolio

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
+import type { _padding } from '#tailwind-config/theme';
 
 </script>
 
 <template>
   <div class="h-screen w-full">
     <div>
-      <div
-        class="flex items-center justify-between border-b border-primary-300 w-full px-6"
-      >
-      <!--  -->
+      <div class="flex items-center justify-between border-b border-primary-300 w-full px-6 relative">
+        <!--  -->
         <div class="w-1/2 flex items-center space-x-6">
           <NuxtLink to="/">
             <img src="~/assets/logo.png" alt="logo" class="h-10 m-5" />
@@ -23,6 +22,26 @@
           <LanguageSwitcher />
         </div>
         <!--  -->
+      </div>
+      <!--  -->
+      <div>
+        <slot/>
+      </div>
+      <!--  -->
+      <div class="fixed bottom-2 w-full flex items-center justify-center space-x-6">
+        <!--  -->
+        <div class="bg-gray-900/20 p-1 rounded-lg flex items-center justify-center space-x-1">
+          <UButton size="lg" :ui="{ padding: 'p-5' }" icon="i-material-symbols:home" to="/" color="black"/>
+          <!--  -->
+          <div class="bg-gray-900/60 p-1 rounded-lg flex items-center justify-center space-x-1">
+            <UButton size="md" :label="$t('general.contact')" to="/contact" color="gray"/>
+            <UButton size="md" :label="$t('general.github')" icon="i-bxl:github"
+            to="https://github.com/Harsisis" target="_blank" color="gray"/>
+            <UButton size="md" :label="$t('general.linkedin')" icon="i-bxl:linkedin"
+            to="https://www.linkedin.com/in/gauthier-cadet-65a426202" target="_blank" color="gray"/>
+          </div>
+          <!--  -->
+        </div>
       </div>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/i18n', '@nuxtjs/tailwindcss'],
+  buildModules: ['nuxt-gsap-module'],
 
   app: {
     head: {
@@ -10,6 +11,46 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
+  },
+
+  gsap: {
+    extraPlugins: {
+      scrollTo: true,
+      scrollTrigger: true
+    },
+    extraEases: {
+      expoScaleEase: true
+    }
+  },
+
+  pageTransition: {
+    name: 'page',
+    mode: 'out-in',
+    css: false,
+
+    beforeEnter(el: any) {
+      this.$gsap.set(el, {
+        opacity: 0
+      })
+    },
+
+    enter(el: any, done: any) {
+      this.$gsap.to(el, {
+        opacity: 1,
+        duration: 0.5,
+        ease: 'power2.inOut',
+        onComplete: done
+      })
+    },
+
+    leave(el: any, done: any) {
+      this.$gsap.to(el, {
+        opacity: 0,
+        duration: 0.5,
+        ease: 'power2.inOut',
+        onComplete: done
+      })
+    }
   },
 
   colorMode: {
